8e10972dd56ad7d093e0d36aeb82d4f128c48b46
Guiaugusto1 <guipiastra68@gmail.com>
2025-07-01 14:47:51 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Button } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Box p={6}>
+      <Heading mb={4}>Sistema de Controle de Loja</Heading>
+      <Button colorScheme="teal">Botão de Teste</Button>
+    </Box>
+  )
+}
+
+export default App
